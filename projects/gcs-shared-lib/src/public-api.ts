1cf0517f004799d3b9ebefd81dc5ff0bb5ed56ed
glennrgordon <148559692+glennrgordon@users.noreply.github.com>
2024-04-19 13:52:20 -0400

    Revert "Add initial source files"
    This reverts commit 00991b6126ef4b33457eb011703b21efeed26691.

--- projects/gcs-shared-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of gcs-shared-lib
- */
-
-export * from './lib/gcs-shared-lib.service';
-export * from './lib/gcs-shared-lib.component';
-export * from './lib/gcs-shared-lib.module';
